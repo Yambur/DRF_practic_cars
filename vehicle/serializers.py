e0945c50d399b9d88a88f082e62b74332c9a8da2
bargest <DIZz92@mail.ru>
2024-05-18 08:25:15 +0300

    add tests

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -11,8 +11,8 @@ class MilageSerializers(serializers.ModelSerializer):
 
 
 class CarSerializer(serializers.ModelSerializer):
-    last_milage = serializers.IntegerField(source='milage.all.first.milage')
-    milage = MilageSerializers(many=True)
+    last_milage = serializers.IntegerField(source='milage.all.first.milage', read_only=True)
+    milage = MilageSerializers(many=True, read_only=True)
 
     class Meta:
         model = Car
